1b8684192d0478c683f5ccf895a5d86d6f0a6814
shubhamwhj <75826441+shubhamwhj@users.noreply.github.com>
2022-03-14 10:44:57 +0530

    Update object_tracking.py

--- object_tracking.py
+++ object_tracking.py
@@ -18,42 +18,26 @@ tracker.init(img, bbox)
 
 print(bbox)
 
-def drawBox(img, bbox):
-    x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
 
-    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
-
-    cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
-
-def goal_track(img, bbox):
-    x, y,w, h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
-
-    #################
-    # ADD CODE HERE #
-    #################
 
 while True:
-    
-    check, img = video.read()   
+    check,img = video.read()   
 
-    # Update the tracker on the img and the bounding box
-    success, bbox = tracker.update(img)
-
-    if success:
-        drawBox(img, bbox)
-    else:
-        cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-
-    #################
-    # ADD CODE HERE #
-    #################
-
-    cv2.imshow("result", img)
+    cv2.imshow("result",img)
             
     key = cv2.waitKey(25)
+
     if key == 32:
-        print("Stopped")
+        print("Stopped!")
         break
+
 
 video.release()
 cv2.destroyALLwindows()
+
+
+
+
+
+
+
